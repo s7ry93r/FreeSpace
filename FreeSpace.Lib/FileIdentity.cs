c5c0443716e334716f4ef31f22ae4f06603255bc
s7ry93r <rkrueger@gmail.com>
2018-10-30 23:40:24 -0400

    getting lib together

---
+++ FreeSpace.Lib/FileIdentity.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeSpace.Lib
+{
+    public class FileIdentity
+    {
+        public string FileExt { get; set; }
+        public string MD5 { get; set; }
+        public long FileSize { get; set; }
+        public List<FileInstance> FileInstances { get; private set; }
+
+        public FileIdentity(FileInfo file)
+        {
+            this.FileExt = file.Extension;
+            this.MD5 = Utility.CalculateMD5(file.FullName);
+            this.FileSize = file.Length;
+            FileInstances = new List<FileInstance>()
+            {
+                new FileInstance()
+                {
+                    FileName = file.Name,
+                    FilePath = file.DirectoryName,
+                    Created = file.CreationTime,
+                    Modified = file.LastWriteTime
+                }
+            };
+        }
+
+        public void AddInstance(FileInstance inst)
+        {
+            FileInstances.Add(inst);
+        }
+    }
+}
---
+++ FreeSpace.Lib/FileManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FreeSpace.Lib
+{
+    public class FileManager
+    {
+        public List<FileIdentity> FileIdentities { get; private set; }
+
+        public void AddFile(FileInfo fi)
+        {
+            var hold = new FileIdentity(fi);
+            var ident = matchedTo(hold);
+            if (ident == null)
+            {
+                FileIdentities.Add(hold);
+            }
+            else
+            {
+                ident.AddInstance(hold.FileInstances[0]);
+            }
+        }
+
+        private FileIdentity matchedTo(FileIdentity hold)
+        {
+            foreach (var ident in FileIdentities)
+            {
+                if (ident.FileExt.ToUpper() == hold.FileExt.ToUpper() &&
+                    ident.FileSize == hold.FileSize &&
+                    ident.MD5 == hold.MD5)
+                {
+                    return ident;
+                }
+            }
+
+            return null;
+        }
+    }
+}
